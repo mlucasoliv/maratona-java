fe5019c54fa3e856a4d95b73610a89a2069034d1
aueis <marcelolucasdeoliveiraa@gmail.com>
2025-01-25 11:55:05 -0300

    feat: Aula de Estruturas Condicionais - Else-If (Concluída)

--- src/academy/devdojo/maratona/java/Aula05EstruturasCondicionais01.java
+++ src/academy/devdojo/maratona/java/Aula05EstruturasCondicionais01.java
@@ -1,6 +1,6 @@
 package academy.devdojo.maratona.java;
 
-public class Aula05EstruturasCondicionais {
+public class Aula05EstruturasCondicionais01 {
     public static void main(String[] args) {
         int idade = 17;
         boolean isAutorizadoComprarBebida = idade >= 18;
@@ -20,5 +20,14 @@ public class Aula05EstruturasCondicionais {
         if(c == false){ //Está comparando.
             System.out.println("Dentro de algo que nunca deve ser feito.");
         }
+
+        // Uso do Else - ElseIf
+        if (isAutorizadoComprarBebida != false){
+            System.out.println("Autorizado a comprar bebida alcólica.");
+        }
+        else{
+            System.out.println("Não pode comprar a bebida.");
+        }
+
     }
 }
---
+++ src/academy/devdojo/maratona/java/Aula05EstruturasCondicionais02.java
@@ -0,0 +1,23 @@
+package academy.devdojo.maratona.java;
+
+public class Aula05EstruturasCondicionais02 { //Bloco da classe
+    public static void main(String[] args) {
+        // idade < 15 categoria infantil
+        // idade >= 15 && idade < 18 categoria juvenil
+        // idade >= 18 categoria adulto
+        int idade = 12;
+        String categoria;
+
+        if (idade <  15){
+            categoria = "Categoria Infantil";
+        }
+        else if(idade >= 15 && idade < 18){
+            categoria = "Categoria Juvenil";
+        }
+        else{
+            categoria = "Categoria Adulta";
+        }
+
+        System.out.println(categoria);
+    }
+}
