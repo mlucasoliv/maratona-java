d104f466643e21d4c59ddf5cb6b73c66473221b6
aueis <marcelolucasdeoliveiraa@gmail.com>
2025-01-25 10:54:48 -0300

    feat: Aula de Estruturas Condicionais - If (Concluída)

--- src/academy/devdojo/maratona/java/Aula05EstruturasCondicionais.java
+++ src/academy/devdojo/maratona/java/Aula05EstruturasCondicionais.java
@@ -2,11 +2,23 @@ package academy.devdojo.maratona.java;
 
 public class Aula05EstruturasCondicionais {
     public static void main(String[] args) {
-        int idade = 20;
-        
-        if (false) {
-            System.out.println("Dentro do if");
+        int idade = 17;
+        boolean isAutorizadoComprarBebida = idade >= 18;
+
+        if (!isAutorizadoComprarBebida) { // É como se fosse comparando, se não é autorizado == falso
+            System.out.println("Não pode comprar a bebida.");
         }
-        System.out.println("Fora do if");
+
+        if (isAutorizadoComprarBebida) {
+            System.out.println("Autorizado a comprar bebida alcólica.");
+        }
+
+        boolean c = true;
+        if(c = false){ //Só está associando, não está fazendo nenhuma comparação.
+            System.out.println("Dentro de algo que nunca deve ser feito.");
+        }
+        if(c == false){ //Está comparando.
+            System.out.println("Dentro de algo que nunca deve ser feito.");
+        }
     }
 }
