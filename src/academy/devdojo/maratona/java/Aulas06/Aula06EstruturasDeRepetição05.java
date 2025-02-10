bc149af5859eacc94f5dec6eb57d46f7dddc133c
aueis <marcelolucasdeoliveiraa@gmail.com>
2025-02-10 11:35:25 -0300

    feat: Aula de Estruturas de Repetição 05 (Concluída)

--- src/academy/devdojo/maratona/java/Aulas06/Aula06EstruturasDeRepetição05.java
+++ src/academy/devdojo/maratona/java/Aulas06/Aula06EstruturasDeRepetição05.java
@@ -1,18 +1,17 @@
 package academy.devdojo.maratona.java.Aulas06;
 
-public class Aula06EstruturasDeRepetição04 {
+public class Aula06EstruturasDeRepetição05 {
     // Dado o valor de um carro, descubra em quantas vezes ele pode ser parcelado
     // Condição valorParcela >= 1000
     public static void main(String[] args) {
-        double valorCarro = 80000;
-        for (int parcela = 1; parcela <= valorCarro ; parcela++) {
+        double valorCarro = 30000;
+        for(int parcela = (int) valorCarro; parcela >= 1; parcela--){
             double valorParcela = valorCarro / parcela;
             if (valorParcela < 1000){
                 continue;
             }
-            System.out.println("Parcela " +parcela+ " R$" +valorParcela);
-        }
-
+            System.out.println("Parcela " +parcela+ " R$ " +valorParcela);
         }
     }
+}
 
