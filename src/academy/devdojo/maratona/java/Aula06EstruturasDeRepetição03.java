d793189c6622ee7909c3aaee0e64bde1d85b2983
aueis <marcelolucasdeoliveiraa@gmail.com>
2025-02-10 10:57:35 -0300

    feat: Aula de Estruturas de Repetição 3 (Concluída)

---
+++ src/academy/devdojo/maratona/java/Aula06EstruturasDeRepetição03.java
@@ -0,0 +1,16 @@
+package academy.devdojo.maratona.java;
+
+public class Aula06EstruturasDeRepetição03 {
+    public static void main(String[] args) {
+        //Imprima os primeiros 25 números e depois pare.
+        int count;
+
+        for(count = 0; count <= 100; count++){
+            System.out.println(count);
+            if(count == 25){
+                break;
+            }
+            }
+        }
+    }
+
