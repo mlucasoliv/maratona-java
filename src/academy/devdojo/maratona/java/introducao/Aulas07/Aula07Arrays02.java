1389ca734c984ad529076575b789621ad01eb81d
aueis <marcelolucasdeoliveiraa@gmail.com>
2025-02-11 12:00:23 -0300

    feat: Aula de Arrays 02 (Concluída)

---
+++ src/academy/devdojo/maratona/java/introducao/Aulas07/Aula07Arrays02.java
@@ -0,0 +1,16 @@
+package academy.devdojo.maratona.java.introducao.Aulas07;
+
+public class Aula07Arrays02 {
+    public static void main(String[] args) {
+        int[] idades = new int[3];
+        // Padrão de inicialização, variáveis declaradas fora do indice.
+        // byte, short, int, long, float e double = 0
+        // char = '\u0000' ' ' (padrão em branco)
+        // boolean = false
+        // String = null
+
+        System.out.println(idades[0]);
+        System.out.println(idades[1]);
+        System.out.println(idades[2]);
+    }
+}
