0e71b9a4b935c5f1fb94014a473dbfc673ac58ba
Marcelo <marcelolucasdeoliveiraa@gmail.com>
2025-05-21 10:24:23 -0300

    teste

--- src/academy/devdojo/maratona/javacore/Bintroducaometodos/dominio/ImpressoraEstudante.java
+++ src/academy/devdojo/maratona/javacore/Bintroducaometodos/dominio/ImpressoraEstudante.java
@@ -2,7 +2,7 @@ package academy.devdojo.maratona.javacore.Bintroducaometodos.dominio;
 
 public class ImpressoraEstudante {
     public void imprime(Estudante estudante){
-        System.out.println("==============");
+        System.out.println("=============");
 
         System.out.println(estudante.nome);
         System.out.println(estudante.idade);
